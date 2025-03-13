346bb0083be7872f23d2cce8360c46f8b3c1fdcd
Victor Oubiña <oubinav75@gmail.com>
2025-03-13 13:41:52 +0100

    Add files via upload

--- dexter-installer_1.0_amd64/usr/lib/dexter-installer/frontend/pages/welcome.py
+++ dexter-installer_1.0_amd64/usr/lib/dexter-installer/frontend/pages/welcome.py
@@ -23,15 +23,9 @@ class WelcomePage:
         """Inicializa la página de bienvenida"""
         self.app = app
         
-        # Configurar CSS para el título con esquinas redondeadas
+        # Cargar el CSS externo
         css_provider = Gtk.CssProvider()
-        css_provider.load_from_data("""
-            .title-header {
-                background-color: #212836;
-                border-top-left-radius: 20px;
-                border-top-right-radius: 20px;
-            }
-        """.encode('utf-8'))
+        css_provider.load_from_path('/usr/share/dexter-installer/styles/style.css')  # Ajusta la ruta según corresponda
         
         # Aplicar el proveedor CSS al contexto de estilo
         screen = Gdk.Screen.get_default()
@@ -57,7 +51,7 @@ class WelcomePage:
         
         # Título de la página
         title = Gtk.Label()
-        title.set_markup("<span foreground='white' size='26000' weight='bold'>{}</span>".format(
+        title.set_markup("<span foreground='white' size='22000' weight='bold'>{}</span>".format(
             _("Bienvenido al instalador de DexterOS")))
         title.set_halign(Gtk.Align.CENTER)
         title.set_hexpand(True)
@@ -73,7 +67,7 @@ class WelcomePage:
         
         # Subtítulo
         subtitle = Gtk.Label()
-        subtitle.set_markup("<span size='large'>{}</span>".format(
+        subtitle.set_markup("<span size='12000' >{}</span>".format(
             _("Este asistente le guiará en la instalación del sistema operativo DexterOS en su equipo.")))
         subtitle.set_halign(Gtk.Align.CENTER)
         subtitle.set_line_wrap(True)
@@ -99,48 +93,78 @@ class WelcomePage:
         
         # Información adicional
         info_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        info_box.set_margin_top(20)
+        info_box.set_margin_top(120)
         info_box.set_margin_start(30)  # Añadir margen adicional izquierdo
         self.content.pack_start(info_box, True, True, 0)
         
         # Información de instalación
         info_label = Gtk.Label()
-        info_label.set_markup("<span size='medium'>{}</span>".format(
+        info_label.set_markup("<span foreground='white' size='12000'>{}</span>".format(
             _("Durante el proceso de instalación, se le solicitará información para configurar su sistema:")))
-        info_label.set_halign(Gtk.Align.START)
+        info_label.set_halign(Gtk.Align.CENTER)
         info_label.set_line_wrap(True)
         info_box.pack_start(info_label, False, False, 0)
         
-        # Lista de aspectos a configurar
+        # Lista de aspectos a configurar - MODIFICADO PARA MOSTRAR DOS COLUMNAS
         points = [
-            _("Idioma y ubicación regional"),
-            _("Zona horaria"),
-            _("Disposición del teclado"),
-            _("Configuración de las particiones del disco"),
-            _("Creación de una cuenta de usuario")
+            _("Idioma y ubicación regional y Zona horaria"), _("Disposición del teclado"),
+            _("Configuración de las particiones del disco"), _("Creación de una cuenta de usuario"),
+            _("Resumen de los cambios"), _("Final y Agradecimiento")
         ]
         
-        for point in points:
+        # Crear un contenedor para las dos columnas
+        columns_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        columns_box.set_halign(Gtk.Align.CENTER)
+        columns_box.set_margin_start(10)
+        info_box.pack_start(columns_box, False, False, 5)
+        
+        # Columna izquierda
+        left_column = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        columns_box.pack_start(left_column, True, True, 0)
+        
+        # Columna derecha
+        right_column = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        columns_box.pack_start(right_column, True, True, 0)
+        
+        # Distribuir los puntos en las dos columnas
+        half = len(points) // 2
+        
+        # Añadir puntos a la columna izquierda
+        for i in range(half):
             point_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-            point_box.set_margin_start(10)
             
             bullet = Gtk.Label()
             bullet.set_markup("<span size='large'>•</span>")
             point_box.pack_start(bullet, False, False, 0)
             
-            label = Gtk.Label(label=point)
+            label = Gtk.Label(label=points[i])
             label.set_halign(Gtk.Align.START)
             label.set_hexpand(True)
             point_box.pack_start(label, True, True, 0)
             
-            info_box.pack_start(point_box, False, False, 5)
+            left_column.pack_start(point_box, False, False, 5)
+        
+        # Añadir puntos a la columna derecha
+        for i in range(half, len(points)):
+            point_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+            
+            bullet = Gtk.Label()
+            bullet.set_markup("<span size='large'>•</span>")
+            point_box.pack_start(bullet, False, False, 0)
+            
+            label = Gtk.Label(label=points[i])
+            label.set_halign(Gtk.Align.START)
+            label.set_hexpand(True)
+            point_box.pack_start(label, True, True, 0)
+            
+            right_column.pack_start(point_box, False, False, 5)
         
         # Nota de recomendación
         note_label = Gtk.Label()
-        note_label.set_markup("<span size='small' style='italic'>{}</span>".format(
+        note_label.set_markup("<span foreground='red' size='12000' style='italic'>{}</span>".format(
             _("Se recomienda que el equipo esté conectado a la red eléctrica durante la instalación.")))
-        note_label.set_halign(Gtk.Align.START)
-        note_label.set_margin_top(20)
+        note_label.set_halign(Gtk.Align.CENTER)
+        note_label.set_margin_bottom(20)
         note_label.set_line_wrap(True)
         info_box.pack_start(note_label, False, False, 0)
     
